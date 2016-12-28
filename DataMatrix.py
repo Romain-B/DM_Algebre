d34e532315e542330c1c48af9edbb9e1b76efebc
rbulteau <romain.bulteau@insa-lyon.fr>
2016-12-28 19:08:46 +0100

    Fix of class removing the parts with pandas bug
    	Could as a result finish the initialisation of L

--- DataMatrix.py
+++ DataMatrix.py
@@ -5,6 +5,7 @@ import csv
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
+from math import exp
 
 class DataMatrix:
   def __init__(self, eps, filename):
@@ -12,35 +13,43 @@ class DataMatrix:
     self.eps = eps
     self.data = pd.read_csv(filename, decimal=",", sep="\s+")
 
+    #Store column titles
     self.headers = list(self.data.columns.values)
 
-    for col in self.headers[1:]:
-    
-      #Turn the string values to float
-      self.data[col] = pd.to_numeric(self.data[col], errors = 'coerce') 
-    
-      #Normalize data
-      self.data[col] = (self.data[col] - self.data[col].mean())/(self.data[col].max()-self.data[col].min())
-
+    self.data = np.array(self.data.values)
     self.nb_el = len(self.data)
 
+    
+    for i, row  in enumerate(self.data[:,1:]) :
+    
+      #Turn the string values to float
+      for j, elt in enumerate(row):  
+        elt = float(elt)
+      #Normalize data
+        self.data[i,j+1] = (self.data[i,j+1] - self.data[:,j+1].mean())/(self.data[:,j+1].max()-self.data[:,j+1].min()) if self.data[:,j+1].max()-self.data[:,j+1].min() != 0 else 0 #(self.data[i,j] - self.data[:,j].mean())
+
+
+    #Initialisation and making of L matrix
     self.L = np.array([[0.0  for _ in range(self.nb_el)] for _ in range(self.nb_el)])
 
     for i in range(self.nb_el):
       for j in range(self.nb_el):
-        self.L[i,j] = self.k(i,j)
+        if i >= j :
+          self.L[i,j] = self.k(i,j)
+          self.L[j,i] = self.L[i,j]
+
+      
 
   def k(self, i ,j):
     """Returns the gaussian similarity kernel between the elements at positions i and j"""
-
-    x = self.data.iloc[i,1:]
-    y = self.data.iloc[j,1:]
-
-    return ((np.linalg.norm(x-y))**2)/self.eps
+    x = self.data[i,1:]
+    y = self.data[j,1:]
+    print ((np.linalg.norm(x-y))**2)/self.eps
+    return exp(((np.linalg.norm(x-y))**2)/self.eps)
 
 
 if __name__ == '__main__':
   
   test = DataMatrix(2, "data_small.csv")
-  print test.k(2,3)
+  
 
