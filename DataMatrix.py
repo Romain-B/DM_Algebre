ce72ebd26da947f20755134da8fd1d04123d380f
rbulteau <romain.bulteau@insa-lyon.fr>
2017-01-02 17:33:16 +0100

    Added load bar for time consuming matrix builds

--- DataMatrix.py
+++ DataMatrix.py
@@ -2,16 +2,33 @@
 #-*- coding:utf-8 -*-
 
 import csv
+import sys
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 from math import exp
 
+def load_bar(now, total):
+  out = "["
+  prog = int(((now*1.0)/(total*1.0)*100))
+
+  for _ in range(prog):
+    out += "#"
+  for _ in range(100-prog):
+    out+= ' '
+  out += "] "+str(prog)+"%"
+  return("\r"+out)
+
+
+
 class DataMatrix:
   def __init__(self, eps, filename):
+
+    print "\nLoading Dataset..."
     
     self.eps = eps
     self.data = pd.read_csv(filename, decimal=",", sep="\s+")
+    print "Done.\n"
 
     #Store column titles
     self.headers = list(self.data.columns.values)
@@ -19,7 +36,7 @@ class DataMatrix:
     self.data = np.array(self.data.values)
     self.nb_el = len(self.data)
 
-    
+    print "\nNormalizing data..."
     for i, row  in enumerate(self.data[:,1:]) :
     
       #Turn the string values to float
@@ -28,21 +45,34 @@ class DataMatrix:
       #Normalize data
         self.data[i,j+1] = (self.data[i,j+1] - self.data[:,j+1].mean())/(self.data[:,j+1].max()-self.data[:,j+1].min()) if self.data[:,j+1].max()-self.data[:,j+1].min() != 0 else 0 #(self.data[i,j] - self.data[:,j].mean())
 
+      sys.stdout.write(load_bar(i+1, self.nb_el))
+    print "\nDone.\n"
 
     #Initialisation and making of L matrix
     self.L = np.array([[0.0  for _ in range(self.nb_el)] for _ in range(self.nb_el)])
 
+    print "\nLoading L matrix...:\n"
     for i in range(self.nb_el):
       for j in range(self.nb_el):
         if i >= j :
           self.L[i,j] = self.k(i,j)
           self.L[j,i] = self.L[i,j]
+      sys.stdout.write(load_bar(i+1,self.nb_el))
+    print "\nDone.\n"
 
     #Initialisation and making of D matrix
     self.D = np.array([[0.0  for _ in range(self.nb_el)] for _ in range(self.nb_el)])
 
+
+    print "Loading D matrix...\n"
     for i in range(self.nb_el):
       self.D[i,i] = self.L[i].sum()
+      sys.stdout.write(load_bar(i+1,self.nb_el))
+    print "\nDone.\n"
+
+    #Iitialisation and making of M matrix with M = D^-1/2 L D^1/2
+
+    print "Loading M matrix...\n"
 
     D_half = np.matrix(np.zeros(self.D.shape))
     D_mhalf = np.matrix(np.zeros(self.D.shape))
@@ -50,12 +80,13 @@ class DataMatrix:
     np.fill_diagonal(D_half, 1/ (self.D.diagonal()**0.5)) 
     np.fill_diagonal(D_mhalf, 1/ (self.D.diagonal()**-0.5))
 
-    #Iitialisation and making of M matrix with M = D^-1/2 L D^1/2
+
     self.M = np.dot(D_mhalf,self.L).dot(D_half)
 
       #Initialisation with M = D^-1 L
       #self.M = np.linalg.inv(self.D).dot(self.L)
 
+    print "\nDone.\n M : \n"
 
 
     print self.M
@@ -71,6 +102,6 @@ class DataMatrix:
 
 if __name__ == '__main__':
   
-  test = DataMatrix(2, "data_small.csv")
-  
+  #test = DataMatrix(2, "data_small.csv")
+  test = DataMatrix(2, "data.csv")
 
