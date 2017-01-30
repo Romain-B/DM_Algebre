3c11768340a68c8ab6337fb9ea1544c7a088c0ad
rbulteau <romain.bulteau@insa-lyon.fr>
2017-01-30 19:36:16 +0100

    BIG update (mostly on notebook)

--- DataMatrix3.py
+++ DataMatrix3.py
@@ -2,124 +2,87 @@
 #-*- coding:utf-8 -*-
 
 import csv
-import sys
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
-from math import exp, sqrt
 from mpl_toolkits.mplot3d import Axes3D
-
-
-def load_bar(now, total):
-  out = "["
-  prog = int(((now*1.0)/(total*1.0)*100))
-
-  for _ in range(prog):
-    out += "#"
-  for _ in range(100-prog):
-    out+= ' '
-  out += "] "+str(prog)+"%" 
-  return "\r"+out
-
+from math import exp, sqrt
 
 
 class DataMatrix:
   def __init__(self, eps, filename, k=2, t=5):
-
-    print "\nLoading Dataset..."
     
     self.eps = eps
+    
+#1. Import des données et normalisation   
+#--------------
     self.data = pd.read_csv(filename, decimal=",", sep="\s+")
-    print "Done.\n"
 
-    #Store column titles
+    #Stockage des en-têtes
     self.headers = list(self.data.columns.values)
-
+    
+    #Stockage des valeurs
     self.data = np.array(self.data.values)
     self.nb_el = len(self.data)
 
-    print "\nNormalizing data..."
+    
     for i, row  in enumerate(self.data[:,1:]) :
-      #Turn the string values to float
-      for j, elt in enumerate(row):  
+      for j, elt in enumerate(row):
+        
+        #Transformation des objets en float
         elt = float(elt)
-      #Normalize data
+        
+        #Normalisation des données
         self.data[i,j+1] = (self.data[i,j+1] - self.data[:,j+1].mean())/(self.data[:,j+1].max()-self.data[:,j+1].min()) if self.data[:,j+1].max()-self.data[:,j+1].min() != 0 else 0 #(self.data[i,j] - self.data[:,j].mean())
-      sys.stdout.write(load_bar(i+1, self.nb_el))
-      
-    print "\nDone.\n"
 
-    #Initialisation and making of L matrix
+
+#2. Construction de la matrice L
+#--------------
     self.L = np.array([[0.0  for _ in range(self.nb_el)] for _ in range(self.nb_el)])
 
-    print "\nLoading L matrix...:\n"
     for i in range(self.nb_el):
       for j in range(self.nb_el):
         if i >= j :
           self.L[i,j] = self.k(i,j)
           self.L[j,i] = self.L[i,j]
-      sys.stdout.write(load_bar(i+1,self.nb_el))
-    print "\nDone.\n"
 
-    #Initialisation and making of D matrix
+#3. Construction de la matrice D
+#--------------
     self.D = np.array([[0.0  for _ in range(self.nb_el)] for _ in range(self.nb_el)])
 
-
-    print "Loading D matrix...\n"
     for i in range(self.nb_el):
       self.D[i,i] = self.L[i].sum()
-      sys.stdout.write(load_bar(i+1,self.nb_el))
-    print "\nDone.\n"
 
-    #Iitialisation and making of Ms matrix with Ms = D^-1/2 L D^-1/2
 
-    print "Loading Ms matrix...\n"
-
+#4. Construction de la matrice M avec M = D^-1/2 L D^1/2
+#--------------
     D_half = np.matrix(np.zeros(self.D.shape))
     D_mhalf = np.matrix(np.zeros(self.D.shape))
 
-    np.fill_diagonal(D_half, 1/ (self.D.diagonal()**0.5)) 
-    np.fill_diagonal(D_mhalf, 1/ (self.D.diagonal()**-0.5))
+    np.fill_diagonal(D_half,  (self.D.diagonal()**0.5))    # -> D^1/2
+    np.fill_diagonal(D_mhalf, (self.D.diagonal()**-0.5))  # -> D^-1/2
+
 
     self.Ms = np.dot(D_mhalf,self.L).dot(D_mhalf)
-
-      #Initialisation with M = D^-1 L
-      #self.M = np.linalg.inv(self.D).dot(self.L)
-    print "\nDone.\n"
-
-
-    print "Computing Eigenvalues of Ms...\n"
-
+    
     w,v = np.linalg.eig(self.Ms)
     le = len(v)
-    #v = np.swapaxes(v,0,1)
-
-    print "w,v\n",w,"\n",v
-
-    #Sort in >>
     
     sor = w.argsort()[::-1]
     v = v[sor]
 
-    w = w[sor]#np.sort(w)[::-1]
-
-    #Initialisation de V et Lambda, Lambda contenant les valeurs propres de M 
-    #et V les vecteurs propres.
-
-
+    w = w[sor]
+    
     self.Lambda = np.array([[0.0 for _ in range(le)] for _ in range(le)])
     for i in range(le):
       self.Lambda[i,i]=w[i]
-      sys.stdout.write(load_bar(i+1,le))
 
     self.V = v
 
-
-    print "\nDone.\n"
-
-
-    #Initialisation of M matrix :
-    self.M = np.dot(D_mhalf,self.V).dot(self.Lambda).dot(self.V.transpose()).dot(D_half)
+      #Construction de M si M = D^-1 L
+      #self.M = np.linalg.inv(self.D).dot(self.L)
+    
+    #self.M = np.dot(D_mhalf,self.V).dot(self.Lambda).dot(self.V.transpose()).dot(D_half)
 
     self.Ksi = np.array([[0.0 for _ in range(self.nb_el)] for _ in range(self.nb_el)])
     self.Phi = np.array([[0.0 for _ in range(self.nb_el)] for _ in range(self.nb_el)])
@@ -133,47 +96,37 @@ class DataMatrix:
     for j in range(self.nb_el):
       self.Ksi[j] = self.V[j]/sqrt(self.D[j,j])
       self.Phi[j] = Vt[j]*sqrt(self.D[j,j])
-      sys.stdout.write(load_bar(j+1,self.nb_el))
-
-    print "\nDone.\n"
-
+        
     self.phi_chelou = []
     
 
     for i in range(k):
       self.phi_chelou.append(w[i+1]**t * self.Ksi[i+1])
 
+    
 
+#5. Définition de la fonction noyau de similarité
+#--------------
+    
   def k(self, i ,j):
     """Returns the gaussian similarity kernel between the elements at positions i and j"""
     x = self.data[i,1:]
     y = self.data[j,1:]
     return exp(-((np.linalg.norm(x-y))**2)/self.eps)
 
-
 def randrange(n, vmin, vmax):
-    '''
-    Helper function to make an array of random numbers having shape (n, )
-    with each number distributed Uniform(vmin, vmax).
-    '''
-    return (vmax - vmin)*np.random.rand(n) + vmin
-
-
-
-
+  '''
+  Helper function to make an array of random numbers having shape (n, )
+  with each number distributed Uniform(vmin, vmax).
+  '''
+  return (vmax - vmin)*np.random.rand(n) + vmin
 
 if __name__ == '__main__':
-  
-  test = DataMatrix(0.1, "data_small.csv", k=3, t=20)
-  print "\nD : \n", test.D
-  print "\nMs :", test.Ms
-  #print "\nPhi.Ksi (should be 1) : ", np.dot(test.Phi, test.Ksi)
-  print "Valeurs propres de Ms :\n", test.Lambda
-  print test.phi_chelou
-  # print "Vecteurs propres de Ms :", test.V
-  #test = DataMatrix(2, "data.csv")
 
 
+  datatest = DataMatrix(1,"data.csv", k=3, t=10)
+  print "\nL :", datatest.L
+  print "\nD :", datatest.D
 
   fig = plt.figure()
   ax = fig.add_subplot(111, projection='3d')
@@ -183,9 +136,9 @@ if __name__ == '__main__':
   # For each set of style and range settings, plot n random points in the box
   # defined by x in [23, 32], y in [0, 100], z in [zlow, zhigh].
   for c, m in [('r', 'o'), ('b', '^')]:
-    xs = test.phi_chelou[0]
-    ys = test.phi_chelou[1]
-    zs = test.phi_chelou[2]
+    xs = datatest.phi_chelou[2]
+    ys = datatest.phi_chelou[1]
+    zs = datatest.phi_chelou[0]
     ax.scatter(xs, ys, zs, c=c, marker=m)
 
   ax.set_xlabel('X Label')
@@ -193,3 +146,4 @@ if __name__ == '__main__':
   ax.set_zlabel('Z Label')
 
   plt.show()
+
